16075f04899ec1026228fd23a5614d82f8656623
Fernly Park <FernlyPark@gmail.com>
2020-07-03 13:48:53 +0200

    add unit test for the utils/containsWhiteSpace function

--- tests/utils/stringUtils.spec.ts
+++ tests/utils/stringUtils.spec.ts
@@ -1,4 +1,4 @@
-import { isAString } from "../../src/utils/stringUtils";
+import { isAString, containsWhiteSpace } from "@App/utils/stringUtils";
 
 describe('testing isAString function', () => {
     it('should detect that the argument is indeed a string', () => {
@@ -7,5 +7,35 @@ describe('testing isAString function', () => {
         const result = isAString('test');
 
         expect(result).toBe(true);
+    });
+
+    it('should detect that a number is not a string', () => {
+        expect.assertions(1);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const test: any = 31;
+        const result = isAString(test);
+
+        expect(result).toBe(false);
+    });
+});
+
+describe('testing containsWhiteSpace function', () => {
+
+    const stringsWithWiteSpaces = [' ', "a test", "tea st", "   ", "with    tab"];
+    it.each(stringsWithWiteSpaces)('should detect that %p contains white space', (stg: string) => {
+        expect.assertions(1);
+
+        const result = containsWhiteSpace(stg);
+
+        expect(result).toBe(true);
+    });
+
+    const stringsWithoutWhiteSpace = ['', 'test']
+    it.each(stringsWithoutWhiteSpace)('should detect that %p does not contains white space', (stg: string) => {
+        expect.assertions(1);
+
+        const result = containsWhiteSpace(stg);
+
+        expect(result).toBe(false);
     });
 });
